12da93b57b73103bfbbc7f8cd944f5b99d981a8a
pasnox <pasnox@eac0da06-7f55-11de-aeba-73c3790875fd>
2009-08-02 14:02:31 +0000

    Initial commit

---
+++ branches/dev/src/editor/MapEditor.h
@@ -0,0 +1,30 @@
+#ifndef MAPEDITOR_H
+#define MAPEDITOR_H
+
+#include "MapItem.h"
+
+class MapEditorItem : public QObject, public MapItem
+{
+	Q_OBJECT
+	
+public:
+	MapEditorItem( QObject* parentObject = 0, QGraphicsItem* parent = 0 );
+	virtual ~MapEditorItem();
+	
+	void setName( const QString& name );
+	void setSize( const QSize& size );
+	void setLayers( const LayersMap& layers );
+	void setLayer( uint id, const LayerMap& layer );
+	void setLayerObjectTile( uint id, const QPoint& pos, const ObjectTile& tile );
+	void setCurrentLayer( int layer, bool exclusive );
+	void removeLayer( int layer );
+	void clearLayers();
+	
+	bool save( const QString& fileName );
+
+protected:
+	ObjectTile objectTile( const QString& name ) const;
+	uint createMapping( const QString& name );
+};
+
+#endif // MAPEDITOR_H
