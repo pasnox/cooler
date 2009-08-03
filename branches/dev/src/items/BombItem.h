8df7b034410d5a0a1fe2b5b6885d920f779979ba
pasnox <pasnox@eac0da06-7f55-11de-aeba-73c3790875fd>
2009-08-03 21:25:03 +0000

    Move items classes to items folder

---
+++ branches/dev/src/items/BombItem.h
@@ -0,0 +1,39 @@
+#ifndef BOMBITEM_H
+#define BOMBITEM_H
+
+#include <QObject>
+#include <QGraphicsPixmapItem>
+
+#include "Globals.h"
+#include "tile/BombTile.h"
+
+class QTimer;
+
+class BombItem : public QObject, public QGraphicsPixmapItem
+{
+	Q_OBJECT
+	
+public:
+	enum { Type = Globals::BombItem };
+	
+	BombItem( const BombTile& bombTile, QObject* parent = 0 );
+	virtual ~BombItem();
+	
+	virtual int type() const;
+	virtual QRectF boundingRect() const;
+	virtual void paint( QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget = 0 );
+
+protected:
+	BombTile mBombTile;
+	int mAnimationStep;
+	QTimer* mAnimationTimer;
+	QTimer* mExplodeTimer;
+	
+	void setTileStep( int step );
+
+protected slots:
+	void animationTimer_timeout();
+	void explodeTimer_timeout();
+};
+
+#endif // BOMBITEM_H
